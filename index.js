88f22d56275f5ec68b9c8a6ef309aa31a079f29c
Kunal jethva <kunaljethva90@gmail.com>
2023-07-26 19:18:33 +0530

    navbar issue

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let menuItems = document.getElementById('menuItems');
 hamburger.addEventListener('click', function () {
   menuItems.className == 'menu flex' ? menuItems.className = 'menu active flex' : menuItems.className = 'menu flex';
   document.getElementById("menu-bar").classList.toggle("change");
-  document.getElementById("nav").classList.toggle("change");
+  document.querySelector(".companyName").classList.toggle("change");
   document.getElementById("menu-bg").classList.toggle("change-bg");
   document.body.style.overflow = 'hidden';
 });
@@ -15,10 +15,10 @@ const companyName = document.querySelector('.companyName');
 window.addEventListener('scroll', function () {
   if (window.scrollY > 0) {
     navbar.classList.add('scrolled');
-    companyName.classList.add('whiteName');
+    companyName.classList.add('change');
   } else {
     navbar.classList.remove('scrolled');
-    companyName.classList.remove('whiteName');
+    companyName.classList.remove('change');
   }
 });
 
